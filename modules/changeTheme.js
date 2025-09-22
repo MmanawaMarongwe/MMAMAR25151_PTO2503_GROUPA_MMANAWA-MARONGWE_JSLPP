98a222e000e2ba219aba4d3a870a4b618098db8a
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-09-22 19:46:32 +0200

    Change logo source with setAttribute in dark mode
    - Update logo src dynamically using setAttribute for light and dark themes
    - Ensure consistent sizing so logo swap appears smooth

--- modules/changeTheme.js
+++ modules/changeTheme.js
@@ -1,9 +1,12 @@
 export const bgColorBtn = document.getElementById("theme-toggle");
+const logoEl = document.getElementById("img-logo");
 
 export function changeTheme() {
   if (bgColorBtn.checked) {
     document.documentElement.classList.add("dark"); //
+    logoEl.setAttribute("src", "assets/logo-dark.svg");
   } else {
     document.documentElement.classList.remove("dark");
+    logoEl.setAttribute("src", "assets/logo-light.svg");
   }
 }
