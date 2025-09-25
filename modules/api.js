cb6c69cb24adec1a0ce3c970058d60aa3db6a9dc
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2025-09-25 12:25:51 +0200

    Add feature to show simple DOM alerts for api loading and error states

--- modules/api.js
+++ modules/api.js
@@ -1,10 +1,12 @@
 // api.js
 export async function getData() {
-  const url = "https://jsl-kanban-api.vercel.app";
-  const response = await fetch(url);
+  const url = "https://jsl-kanban-api.vercel.apps";
   try {
+    showTempAlert("Loading tasks…");
     const response = await fetch(url);
     if (!response.ok) {
+      const erMsg = "API failed to fetch";
+      alert(erMsg);
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
@@ -13,9 +15,28 @@ export async function getData() {
     // Save to localStorage
     localStorage.setItem("tasks", JSON.stringify(data));
 
-    console.log("Tasks saved to localStorage:", data);
     return data;
   } catch (error) {
+    showTempAlert("Failed to load tasks. Please check your connection.", 5000);
     console.error("Failed to fetch tasks:", error);
   }
+}
+
+function showTempAlert(msg, duration = 1000) {
+  const el = document.createElement("div");
+  el.textContent = msg;
+  el.style.position = "fixed";
+  el.style.top = "60px";
+  el.style.left = "50%";
+  el.style.transform = "translateX(-50%)";
+  el.style.background = "#828fa3";
+  el.style.color = "#fff";
+  el.style.padding = "20px 20px";
+  el.style.borderRadius = "5px";
+  el.style.zIndex = 1000;
+  document.body.appendChild(el);
+
+  setTimeout(() => {
+    el.remove();
+  }, duration);
 }
